6ddec5f986e5f71eff3f665aa4db69863340265a
Carl Malmz <callem217@gmail.com>
2022-07-27 21:26:32 +0200

    change postgres format

--- db/db.go
+++ db/db.go
@@ -2,9 +2,9 @@ package db
 
 import (
 	"context"
-	"fmt"
 	"os"
 
+	"entgo.io/ent/dialect"
 	"github.com/dtekcth/dtek-api/ent"
 	_ "github.com/lib/pq"
 	"github.com/rs/zerolog/log"
@@ -13,13 +13,7 @@ import (
 var db *ent.Client
 
 func Init() {
-	host := os.Getenv("DB_HOST")
-	port := os.Getenv("DB_PORT")
-	user := os.Getenv("DB_USER")
-	password := os.Getenv("DB_PASSWORD")
-	dbname := os.Getenv("DB_NAME")
-
-	client, err := ent.Open("postgres", fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s", host, port, user, dbname, password))
+	client, err := ent.Open(dialect.Postgres, os.Getenv("DB_DSN"))
 	if err != nil {
 		log.Fatal().Err(err).Msg("failed opening connection to database")
 	}
